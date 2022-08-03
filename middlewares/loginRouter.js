0fc01564322e879958fe6d16602bb68378381989
WBomfim <willianbomfim@hotmail.com>
2022-08-03 20:47:25 -0300

    Feat: criado o middleware para validar o email recebido na resquisição de login

--- middlewares/loginRouter.js
+++ middlewares/loginRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { generateToken } = require('../helpers/generateToken');
+const { validateEmail } = require('./validateEmail');
 
 const router = express.Router();
 
-router.post('/', async (_req, res) => {
+router.post('/', validateEmail, async (_req, res) => {
   const token = generateToken();
   res.status(200).json({ token });
 });
