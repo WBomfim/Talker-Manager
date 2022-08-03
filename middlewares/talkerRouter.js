aa8f29694a28d9f290cbf6eceabf4423da08be60
WBomfim <willianbomfim@hotmail.com>
2022-08-03 19:03:35 -0300

    Feat: Criado o endpoint GET /talker/:id que retorna o palestrante selecionado pelo id.

--- middlewares/talkerRouter.js
+++ middlewares/talkerRouter.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData();
+    const selectedTalker = data.find((talker) => talker.id === Number(id));
+    if (!selectedTalker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(selectedTalker);
+  } catch (err) {
+    return res.status(500).json({ message: 'Erro ao acessar o banco de dados' });
+  }
+});
+
 module.exports = router;
