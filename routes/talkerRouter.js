66bffba04342c8247d1be37f3a92d7d28c6652a7
WBomfim <willianbomfim@hotmail.com>
2022-08-04 00:19:28 -0300

    Feat: Criado o middleware para verificação da chave talk enviado no post para o endpoint /talker

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -4,12 +4,18 @@ const talkerController = require('../controllers/talkers');
 const validateToken = require('../middlewares/validateToken');
 const validateName = require('../middlewares/validateName');
 const validateAge = require('../middlewares/validateAge');
+const validateTalk = require('../middlewares/validateTalk');
 
 const router = express.Router();
 
 router.get('/', handleErrors(talkerController.getAllTalkers));
 
-router.post('/', validateToken, validateName, validateAge, handleErrors());
+router.post('/',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  handleErrors());
 
 router.get('/:id', handleErrors(talkerController.getTalkerById));
 
