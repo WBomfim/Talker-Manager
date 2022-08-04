168e9c07e67ca2f59d1858ca2a0e30211ae66f20
WBomfim <willianbomfim@hotmail.com>
2022-08-03 23:23:28 -0300

    Feat: Criado o middleware para validação do token para o post no endpoint /talkers.

--- middlewares/talkerRouter.js
+++ middlewares/talkerRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const handleErrors = require('./handleErrors');
 const talkerController = require('../controllers/talkers');
+const validateToken = require('./validateToken');
 
 const router = express.Router();
 
 router.get('/', handleErrors(talkerController.getAllTalkers));
+
+router.post('/', validateToken, handleErrors());
 
 router.get('/:id', handleErrors(talkerController.getTalkerById));
 
