f7216979e36e1380ff5081989fd562bfda273dd1
WBomfim <willianbomfim@hotmail.com>
2022-08-04 00:08:45 -0300

    Feat: Criado o middleware para verificação da idade enviado no post para o endpoint /talker

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -3,12 +3,13 @@ const handleErrors = require('../middlewares/handleErrors');
 const talkerController = require('../controllers/talkers');
 const validateToken = require('../middlewares/validateToken');
 const validateName = require('../middlewares/validateName');
+const validateAge = require('../middlewares/validateAge');
 
 const router = express.Router();
 
 router.get('/', handleErrors(talkerController.getAllTalkers));
 
-router.post('/', validateToken, validateName, handleErrors());
+router.post('/', validateToken, validateName, validateAge, handleErrors());
 
 router.get('/:id', handleErrors(talkerController.getTalkerById));
 
