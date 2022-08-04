f0539b2bf68ebaec7596d45afa8f81e1dfba0d6e
WBomfim <willianbomfim@hotmail.com>
2022-08-04 00:03:45 -0300

    Feat: Criado o middleware para verificação do nome enviado no post para o endpoint /talker

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const handleErrors = require('../middlewares/handleErrors');
 const talkerController = require('../controllers/talkers');
 const validateToken = require('../middlewares/validateToken');
+const validateName = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/', handleErrors(talkerController.getAllTalkers));
 
-router.post('/', validateToken, handleErrors());
+router.post('/', validateToken, validateName, handleErrors());
 
 router.get('/:id', handleErrors(talkerController.getTalkerById));
 
